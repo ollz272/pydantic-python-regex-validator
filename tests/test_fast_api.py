93d7fa51e7404f2496bf7bfe53fa3d85ab17a9a3
Oliver Parker <46482091+ollz272@users.noreply.github.com>
2023-08-16 20:59:17 +0100

    feat(fastapi): add some fastapi utils (#5)
    * test: add fastapi tests
    
    * fix: fixes
    
    * fix: add .idea
    
    * fix: remove .idea
    
    * feat(fastapi): adds some fastapi utility functions
    
    * test: some extra tests
    
    * fix: add testing to ci
    
    * fix: ci file??
    
    * fix: formatting
    
    * fix: testing
    
    ---------
    
    Co-authored-by: Oli Parker <oliver.parker@gridedge.co.uk>

--- tests/test_fast_api.py
+++ tests/test_fast_api.py
@@ -1,28 +1,44 @@
 """Tests for functionality with fast api."""
-from platform import python_version
-from typing import Optional
-
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
-from pydantic_python_regex_validator.regex import Regex
-
-if python_version()[:3] == "3.8":
-    from typing_extensions import Annotated
-else:
-    from typing import Annotated
+from pydantic_python_regex_validator.fast_api import RegexBody, RegexQuery
 
 app = FastAPI()
 
 
 @app.get("/foo")
-def foo(param: Annotated[str, Query(), Regex("^foo")]):
+def foo_get(param: RegexQuery(pattern="^foo")):
+    """Test foo endpoint."""
+    return param
+
+
+@app.get("/foo-param")
+def foo_get_with_query_param(param: RegexQuery(pattern="^foo", query_params={"alias": "aParam"})):
+    """Test foo endpoint."""
+    return param
+
+
+@app.post("/foo")
+def foo_post(param: RegexBody(pattern="^foo")):
+    """Test foo endpoint."""
+    return param
+
+
+@app.post("/foo-param")
+def foo_post_with_param(param: RegexBody(pattern="^foo", body_params={"embed": True})):
     """Test foo endpoint."""
     return param
 
 
 @app.get("/bar")
-def bar(param: Annotated[Optional[str], Query(), Regex("^bar", allow_none=True)] = None):
+def bar_get(param: RegexQuery(pattern="^bar", allow_none=True) = None):
+    """Test bar endpoint."""
+    return param
+
+
+@app.post("/bar")
+def bar_post(param: RegexBody(pattern="^bar", allow_none=True) = None):
     """Test bar endpoint."""
     return param
 
@@ -30,21 +46,55 @@ def bar(param: Annotated[Optional[str], Query(), Regex("^bar", allow_none=True)]
 client = TestClient(app)
 
 
-def test_correct_regex():
+def test_correct_regex_get():
     """Tests fastapi parses the string correctly."""
     response = client.get("/foo", params={"param": "foo"})
     assert response.status_code == 200
     assert response.json() == "foo"
 
 
-def test_incorrect_regex():
+def test_correct_regex_get_with_alias():
+    """Tests fastapi parses the string correctly."""
+    response = client.get("/foo-param", params={"aParam": "foo"})
+    assert response.status_code == 200
+    assert response.json() == "foo"
+
+
+def test_incorrect_regex_get():
     """Tests fastapi rejects the incorrect regex."""
     response = client.get("/foo", params={"param": "bar"})
     assert response.status_code == 422
 
 
-def test_correct_regex_with_missing():
+def test_correct_regex_with_missing_get():
     """Tests fastapi allows none when allowed."""
     response = client.get("/bar")
+    assert response.status_code == 200
+    assert response.json() is None
+
+
+def test_correct_regex_post():
+    """Tests fastapi parses the string correctly."""
+    response = client.post("/foo", json="foo")
+    assert response.status_code == 200
+    assert response.json() == "foo"
+
+
+def test_correct_regex_post_with_embed():
+    """Tests fastapi parses the string correctly."""
+    response = client.post("/foo-param", json={"param": "foo"})
+    assert response.status_code == 200
+    assert response.json() == "foo"
+
+
+def test_incorrect_regex_post():
+    """Tests fastapi rejects the incorrect regex."""
+    response = client.post("/foo", json={"param": "bar"})
+    assert response.status_code == 422
+
+
+def test_correct_regex_with_missing_post():
+    """Tests fastapi allows none when allowed."""
+    response = client.post("/bar")
     assert response.status_code == 200
     assert response.json() is None
